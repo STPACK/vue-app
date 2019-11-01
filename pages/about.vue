9c6fafb21dbd9a1a30ccc2418c8b42cdfe5b1426
STPACk <sattha.prat@hotmail.com>
2019-11-01 11:36:23 +0700

    create next

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="con-center">
+   <h1>About</h1>
+  </div>
+</template>
+<style>
+  .con-center {
+     text-align: center;
+     margin-top: 1rem;
+  }
---
+++ pages/pack.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="con">
+        <h1>Hello  {{name}}</h1>
+        <input type="text" v-model="name">
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name:'STPACK',
+        }
+    }
+}
+</script>
+
+<style>
+  .con {
+     text-align: center;
+     margin-top: 1rem;
+  }
+  </style>
