5b6660dcd432658a5cf43f003e853861a17b1229
STPACk <sattha.prat@hotmail.com>
2019-11-01 13:19:58 +0700

    methods

--- pages/pack.vue
+++ pages/pack.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="con">
         <h1>Hello  {{name}}</h1>
-        <input type="text" v-model="name">
+        <input type="text" v-model="name"> <br><br>
+        <h2>enter change   {{name}}</h2>
+        <input type="text" v-model="Nname" v-on:keyup.enter="changename">
+        <button @click="changename">Click to change</button>
     </div>
     
 </template>
@@ -9,9 +12,20 @@
 <script>
 export default {
     data(){
+       
         return{
             name:'STPACK',
+            Nname:''
+
         }
+    },
+    methods:{
+        changename(){
+            this.name=this.Nname,
+            this.Nname=''
+
+        }
+
     }
 }
 </script>
